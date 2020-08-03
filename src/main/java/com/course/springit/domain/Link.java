a61fb71bd439fac8813c1ae12958ceed91116303
Anna Polakova <ap@weissenrieder.com>
2020-08-03 16:17:58 +0300

    Created entities and refactored with Lombok.

---
+++ src/main/java/com/course/springit/domain/Link.java
@@ -0,0 +1,24 @@
+package com.course.springit.domain;
+
+import com.sun.istack.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Link {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NotNull
+    private String title;
+    @NotNull
+    private String url;
+}
