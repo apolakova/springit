ee33e8485dc21cc3db933fa5f8163ce154a4ea86
Anna Polakova <ap@weissenrieder.com>
2020-10-02 14:53:32 +0300

    Commented command line runner out.

--- src/main/java/com/course/springit/SpringitApplication.java
+++ src/main/java/com/course/springit/SpringitApplication.java
@@ -1,15 +1,14 @@
 package com.course.springit;
 
 import com.course.springit.config.SpringitProperties;
+import com.course.springit.repository.CommentRepository;
+import com.course.springit.repository.LinkRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableConfigurationProperties(SpringitProperties.class)
@@ -17,19 +16,12 @@ public class SpringitApplication {
 
     private static final Logger log = LoggerFactory.getLogger(SpringitProperties.class);
 
-    @Autowired
-    private SpringitProperties springitProperties;
-
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
-        System.out.println("Welcome to Springit!");
     }
 
-    @Bean
-    @Profile("dev")
-    CommandLineRunner runner() {
-        return args -> {
-            System.out.println(springitProperties.getWelcomeMsg());
-        };
+    //@Bean
+    CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository) {
+        return null;
     }
 }
